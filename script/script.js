09f69a75354d00bcd8f482d02bd50c70af6aa523
Sunaljabili <you@example.com>
2021-07-31 01:30:21 +0400

    Tasks Comploted

---
+++ script/script.js
@@ -0,0 +1,34 @@
+// Task1
+
+// let number = prompt("Ededi daxil edin");
+
+// if (number>0){
+//     console.log("Bu eded musbetdir")
+// }else if (number <0){
+//     console.log("Bu eded menfidir")
+// } else{
+//     console.log("Bu eded sifirdir")
+// }
+
+// Task2
+
+// let number = prompt("Reqem daxil edin");
+
+
+// result = number/100;
+
+// if (result>1 && result<=9){
+//     console.log("Bu eded 3 reqemlidir")
+// }else{
+//     console.log("Bu eded 3 reqemli deyil")
+// }
+
+// Task3
+
+// let number = prompt("Eded daxil edin:")
+
+// if(number%21==0){
+//     console.log("Bu eded hem 7ye hemde 3e bolunur")
+// }else{
+//     console.log("Bu eded 7ye ve 3 e bolunmur")
+// }
